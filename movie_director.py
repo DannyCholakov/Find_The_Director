066ac38f977fddae8b9048151a32642c84a0c570
Daniel Cholakov <danielcholakov133@gmail.com>
2024-10-22 14:01:03 +0200

    Movie Director Program with IMDB DB

---
+++ movie_director.py
@@ -0,0 +1,18 @@
+from imdb import IMDb
+
+ia = IMDb()
+movie_name = input( "Enter the movie name: ")
+movies = ia.search_movie(movie_name)
+
+if movies:
+    movie = movies[0]
+    ia.update(movie)
+    directors = movie.get('directors')
+    if directors:
+        print ("Director(s):")
+        for director in directors:
+            print(director['name'])
+    else:
+        print("No director information found.")
+else:
+    print( "Movie not found.")
